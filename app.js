b630f7919d89c434973a1f1872bb472b74a8b114
Samuel <sam@beats.com>
2023-01-31 01:17:06 -0500

    restrict colors to purples

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function randomRGB() {
-    const r = Math.floor(Math.random() * 20)
-    const g = Math.floor(Math.random() * 35)
-    const b = Math.floor(Math.random() * 150)
+    const r = Math.floor(Math.random() * 256)
+    const g = 0 //Math.floor(Math.random() * 0)
+    const b = Math.floor(Math.random() * 256)
     return `rgb(${r}, ${g}, ${b})`
 }
 
@@ -10,4 +10,4 @@ const intervalid = setInterval(function() {
     for (let letter of letters) {
         letter.style.color = randomRGB();
     }
-}, 700);
+}, 1000);
